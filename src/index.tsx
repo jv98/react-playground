90309ab90c2c2fdf31c7d28edb07094fba7be35a
jv98 <jacob@vahlen.se>
2021-02-22 13:50:27 +0100

    start

--- src/index.tsx
+++ src/index.tsx
@@ -1,8 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import App from './components/App';
 
 
 ReactDOM.render(
-    <h1>HELLO</h1>,
+    <App/>,
     document.getElementById('app-container')
 );
